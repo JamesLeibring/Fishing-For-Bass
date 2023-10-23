d73de4b370702fbcaafe643ede85795912064f64
James <jleibring@gmail.com>
2023-10-22 23:02:04 -0400

    Player Class Created

--- src/player.py
+++ src/player.py
@@ -1,17 +1,27 @@
-# The Player Class
+import pygame
+
+import config as config_
 
 class Player:
-    def __init__(self, player):
-        self.player = player
-        self.color = 'blue'
+    def __init__(self, player, name):
+        # Config Objects
+        self.config = config_.config['Buttons']['Players']
 
+        self.name = name
+
+        # The player number    
+        self.player = player
+
+        # The territories and costs owned by the player
         self.territories = []
         self.coasts = []
 
-        self.units = {
-            'Land': [],
-            'Sea': [],
-            'Air': []   
-        }
-
+        # The resources of the player
         self.resources = [0, 0, 0, 0]
+
+        # The button for the player
+        self.button = pygame.Rect(self.config['start'][self.player][0], self.config['start'][self.player][1], self.config['width'], self.config['height'])
+    
+    # Returns true if the point is on the button
+    def inside(self, x, y):
+        return self.button.collidepoint(x, y)
