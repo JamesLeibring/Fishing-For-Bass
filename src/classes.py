a74f76a8d3ad87cc89fd6fd69efa1e1d1ad3df6d
James <jleibring@gmail.com>
2023-12-23 15:07:44 -0500

    Config Class complete

--- src/classes.py
+++ src/classes.py
@@ -1,7 +1,7 @@
 import configparser, logging, pygame
 
 # A config object is simply an object that uses values from the config
-class ConfigObject:
+class ConfigPygame:
   def __init__(self, config:configparser.ConfigParser) -> None:
     # The actual config object
     self.config = config
@@ -10,7 +10,7 @@ class ConfigObject:
   def image(self, image:str) -> pygame.Surface:
     url = self.config['IMAGES'][image]
 
-    size = self.config.getint('IMAGES', 'size')
+    size = self.getint('IMAGES', 'size')
 
     return pygame.transform.scale(pygame.image.load(url), (size, size))
 
@@ -20,6 +20,10 @@ class ConfigObject:
 
     return pygame.Color(rgb[0], rgb[1], rgb[2], rgb[3] if len(rgb) == 4 else 255)
   
+  # Returns a pygame Font object given a font size
+  def font(self, size:str) -> pygame.font.Font:
+    return pygame.font.Font(self.config['FONTS']['font'], self.getint('FONTS', size))
+  
   # Returns a pygame rectangle given name
   def rect(self, rect:str) -> pygame.Rect:
     topleft = self.gettuple(rect, 'topleft')
@@ -28,6 +32,18 @@ class ConfigObject:
     width_height = (bottomright[0] - topleft[0], bottomright[1] - topleft[1])
 
     return pygame.Rect(topleft, width_height)
+  
+  # Returns player 
+  def player(self, player:str) -> dict:
+    return self.getdict('PLAYERS', player)
+  
+  # Returns a dictionary with unit information
+  def unit(self, unit:str) -> dict:
+    return self.getdict('UNITS', unit)
+  
+  # Returns a dictionary with territory information
+  def territory(self, territory:str) -> dict:
+    return self.getdict('TERRITORIES', territory)
 
   # Parses a string in form '1-9' into an int
   def getint(self, section:str, option:str) -> int:
@@ -36,3 +52,31 @@ class ConfigObject:
   # Parses a string in form 'x.y' into a tuple of form (x, y)
   def gettuple(self, section:str, option:str) -> tuple:
     return tuple(int(x) for x in self.config[section][option].split('.'))
+  
+  # Parses a dictionary in form 'key:value, key:a|b|c|d, key:a.b'
+  def getdict(self, section:str, option:str) -> dict:
+    d = dict()
+
+    pairs = self.config[section][option].split(',')
+
+    for pair in pairs:
+      key, value = pair.split(':')
+
+      # Case of list
+      if value.startswith('[') and value.endswith(']'):
+        value = value[1:-1].split('|')
+
+        for v in value:
+          v = tuple(int(x) for x in v.split('.'))
+
+      # Case of tuple
+      elif value.startswith('(') and value.endswith(')'):
+        value = tuple(int(x) if x.isdigit() else x for x in value[1:-1].split('.'))
+      
+      # Case of int/string
+      else:
+        value = int(value) if value.isdigit() else value
+
+      d[key] = value
+    
+    return d
